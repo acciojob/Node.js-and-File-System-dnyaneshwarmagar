b6b7cb5bca47435f5b984b0e0ea6eee160e4a434
dnyaneshwarmagar <dnyaneshwarmagar276@gmail.com>
2024-05-21 04:51:11 +0530

    done

--- main.js
+++ main.js
@@ -1,30 +1,39 @@
 const fs = require('fs');
-const path = require('path');
+const readline = require('readline');
+const path= require("path")
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
 
-if (process.argv.length < 4) {
-  console.log('Usage: node app.js <filename> <wordToRemove>');
-  process.exit(1);
-}
+rl.question('Enter the filename: ', (filename) => {
+  rl.question('Enter the word to remove: ', (word) => {
+    const filePath = path.join(__dirname, filename);
+    removeWordFromFile(filePath, word);
+    rl.close();
+  });
+});
 
-const [filename, wordToRemove] = process.argv.slice(2);
-
-const filePath = path.join(__dirname, filename);
-
-fs.readFile(filePath, 'utf8', (err, data) => {
-  if (err) {
-    console.error(`Error reading the file: ${err}`);
-    return;
-  }
-
-  const regex = new RegExp(`\\b${wordToRemove}\\b`, 'g');
-  const modifiedData = data.replace(regex, '');
-
-  fs.writeFile(filePath, modifiedData, 'utf8', err => {
+function removeWordFromFile(filePath, wordToRemove) {
+  // Read the file contents
+  fs.readFile(filePath, 'utf8', (err, data) => {
     if (err) {
-      console.error(`Error writing to the file: ${err}`);
+      console.error(`Error reading file: ${err.message}`);
       return;
     }
 
-    console.log(`Removed ${wordToRemove} from ${filename}`);
+    // Remove all occurrences of the specified word
+    const regex = new RegExp(wordToRemove, 'g');
+    const modifiedContent = data.replace(regex, '');
+
+    // Write the modified content back to the same file
+    fs.writeFile(filePath, modifiedContent, 'utf8', (err) => {
+      if (err) {
+        console.error(`Error writing to file: ${err.message}`);
+        return;
+      }
+
+      console.log(`All occurrences of "${wordToRemove}" have been removed from the file.`);
+    });
   });
-});
+}
