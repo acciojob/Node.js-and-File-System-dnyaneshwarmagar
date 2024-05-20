ff34531dcce9e46a998d757ba3675868636decae
dnyaneshwarmagar <dnyaneshwarmagar276@gmail.com>
2024-05-21 04:05:21 +0530

    done

--- main.js
+++ main.js
@@ -8,7 +8,31 @@ const rl = readline.createInterface({
 
 rl.question('Enter the filename: ', (filename) => {
   rl.question('Enter the word to remove: ', (word) => {
-    // TODO: Implement this function
+    removeWordFromFile(filename, word);
     rl.close();
   });
 });
+
+function removeWordFromFile(filePath, wordToRemove) {
+  // Read the file contents
+  fs.readFile(filePath, 'utf8', (err, data) => {
+    if (err) {
+      console.error(`Error reading file: ${err.message}`);
+      return;
+    }
+
+    // Remove all occurrences of the specified word
+    const regex = new RegExp(wordToRemove, 'g');
+    const modifiedContent = data.replace(regex, '');
+
+    // Write the modified content back to the same file
+    fs.writeFile(filePath, modifiedContent, 'utf8', (err) => {
+      if (err) {
+        console.error(`Error writing to file: ${err.message}`);
+        return;
+      }
+
+      console.log(`All occurrences of "${wordToRemove}" have been removed from the file.`);
+    });
+  });
+}
